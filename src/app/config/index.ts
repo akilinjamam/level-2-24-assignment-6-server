d07989fd9e3effb0802704083cd229e0bf5c76e2
akilinjamam <akilinjamam@gmail.com>
2024-09-30 16:49:12 +0600

    file-updated

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -9,10 +9,5 @@ export default {
   bcrypt_salt_round: process.env.BCRYPT_SALT_ROUND,
   default_password: process.env.DEFAULT_PASS,
   jwt_access_secret: process.env.JWT_ACCESS_SECRET,
-  jwt_refresh_secret: process.env.JWT_REFRESH_TOKEN,
   jwt_access_expiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
-  jwt_refresh_expiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
-  store_id: process.env.STORE_ID,
-  store_pass: process.env.STORE_PASSWORD,
-  smtp_username: process.env.SMTP_USERNAME,
 };
