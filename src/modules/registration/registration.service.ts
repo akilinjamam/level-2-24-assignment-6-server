8a8fbae93f9364aac0b025d20aadf441d9f462f3
akilinjamam <akilinjamam@gmail.com>
2024-10-06 02:42:59 +0600

    file-updated

--- src/modules/registration/registration.service.ts
+++ src/modules/registration/registration.service.ts
@@ -100,7 +100,7 @@ const passwordRecovery = async (payload: TRecoveryPassword) => {
     '10m',
   );
 
-  const htmlUiLink = `https://level-2-24-assignment-6-clients.vercel.app/recoveryPassword?token=${resetToken}`;
+  const htmlUiLink = `https://level-2-24-assignment-6-clients.vercel.app/recoveryPassword/?token=${resetToken}`;
 
   try {
     await sendEmail(findUser?.email, htmlUiLink);
